d6bf7befdfb8f0070f1b6a19b4b2fe4eac6953b0
Lucas Prestes <108070512+LucasP0@users.noreply.github.com>
2023-05-14 00:12:23 -0300

    uhhu

---
+++ Sidebar Menu/index.js
@@ -0,0 +1,4 @@
+const body = document.querySelectorAll("body"),
+      sidebar = body.querySelectorAll(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      searchBnt = body.querySelector(".search-box")
